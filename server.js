6c043cb2aa12662c770dcb88058c6e867a8a11ea
Samuel Gordalina <samuel.gordalina@gmail.com>
2017-07-02 21:27:22 +0100

    Add html file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var server = express()
 
 server.get('/', (req, res) => {
-  res.send('Hello from express');
+  res.sendFile(`${__dirname}/app/index.html`);
 })
 
 server.listen(3000);
