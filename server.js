775f0aa4925dd5ebbdd5fe69417efa460c2ca6ff
Samuel Gordalina <samuel.gordalina@gmail.com>
2017-07-02 22:01:56 +0100

    Add frontend

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
-var server = express()
+var server = express();
+var http = server.listen(3000);
+var io = require('socket.io')(http);
 
 server.get('/', (req, res) => {
   res.sendFile(`${__dirname}/app/index.html`);
-})
+});
 
-server.listen(3000);
+io.on('connection', (socket) => {
+  console.log(`> New connection: ${socket.id}`);
+  socket.emit('connected', { timestamp: new Date() });
+  socket.on('message', (data) => {
+    console.log(`< ${socket.id}: ${data.text}`);
+  });
+});
